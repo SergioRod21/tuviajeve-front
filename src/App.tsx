fff03c1511a62b88010192827ca13b2d897c893f
sergiorod21 <sergiorod.dev@gmail.com>
2025-01-30 13:57:10 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Header from './components/Header'
+function App() {
+  return (
+    <div className='w-full min-h-screen flex flex-col bg-gray-900'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+function Header() {
+    return (
+        <div className='flex flex-col justify-center items-center p-4'>
+            <h1 className="text-white text-4xl font-semibold">TuViajeVE</h1>
+            <p className="text-gray-500 font-medium text-center pt-2">Una simple solución para comparar el precio de las distintas aplicaciones de movilidad de Venezuela</p>
+        </div>
+    )
+}
+
+export default Header
